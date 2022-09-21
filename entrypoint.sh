45db6eb57fb421352bfa582066311571fab885b5
Murillo Mamud <murillomamud@gmail.com>
2022-09-21 19:03:02 -0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
 
-cf api "$INPUT_CF_API"
-cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
+#cf api "$INPUT_CF_API"
+#cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
+cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
+cf apps
 
-sh -c "cf $*"  
+#sh -c "cf $*"  
