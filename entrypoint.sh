824a0740eb35d50f94cf9dc44996c718ba6dfffb
Murillo Mamud <murillomamud@gmail.com>
2022-09-21 19:40:02 -0300

    chmod in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@
 #cf api "$INPUT_CF_API"
 #cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
+chmod -R 777 ./
+
 cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
 cf apps
 
