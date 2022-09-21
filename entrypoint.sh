668ecc6142093147ec924adb3170727067947074
Murillo Mamud <murillomamud@gmail.com>
2022-09-21 20:32:46 -0300

    ADD VARS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh -l
 
+CF_API=$1
+CF_USER=$2
+CF_PASS=$3
+CF_ORG=$4
+CF_SPACE=$5
+CF_COMMAND=$6
 
 #cf api "$INPUT_CF_API"
 #cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
 
 
-cf login -a "123" -u "123" -p "123"
+cf login -a $CF_API -u $CF_USER -p $CF_PASS
 cf apps
 
 #sh -c "cf $*"  
